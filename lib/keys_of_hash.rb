5ae37349a2d998a32fdab33eb0a1f331be2d0111
Leizl Samano <leizl.samano@gmail.com>
2018-12-04 22:36:31 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    # Take arguments, these are values.
+    array_of_keys = []
+    # Checks the given hash for the values.
+    each do |key, value|
+      if arguments.include?(value)
+        # Stores the keys of the matching values.
+        array_of_keys << key
+      end
+    end
+    # Returns array of keys.
+    array_of_keys
   end
 end
